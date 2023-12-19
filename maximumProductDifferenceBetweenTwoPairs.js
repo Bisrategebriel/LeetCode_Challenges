8f014f91ca2924398b088cede4a775a79a292100
Bisrategebriel Fisseha <bisrate22@gmail.com>
2023-12-19 10:54:23 +0300

    Complete 1913. Maximum Product Difference Between Two Pairs

---
+++ maximumProductDifferenceBetweenTwoPairs.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProductDifference = function (nums) {
+  let maximum = nums[0], secondMaximum = nums[0], minimum = nums[0], secondMinimum = nums[0];
+
+  //   sort array in ascending order
+  nums.sort((a, b) => (a - b))
+
+  // return the product difference 
+  return ((nums[nums.length - 1] * nums[nums.length - 2]) - (nums[0] * nums[1]))
+
+};
